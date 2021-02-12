7d7b0a57cdb89fdd4eae5b3745f40c7c440dc75f
Zerui Wei <wezerui@umich.edu>
2021-02-11 20:41:34 -0500

    first commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,15 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, callback)
+}
+
+function callback(result) {
+	console.log("call back called")
+	var curHtml = 
+	'<img src = "' + result['image'] + ' " class = "detailsImage">' + 
+	'<p>' + result['title'] + '</p>' + 
+	'<p>' + result['date'] + '</p>' +
+	'<p>' + result['summary'] + '</p>'
+	$("#project" + result['id'] + " .details").html(curHtml);
 }
